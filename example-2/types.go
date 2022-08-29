6005aca51669e563f8b0bd30edce3f5e0bad7f9a
Jose Nidhin <jose.nidhin@dtone.com>
2022-08-29 15:25:58 +0800

    Improve pretty print function

--- example-2/types.go
+++ example-2/types.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"text/tabwriter"
 
@@ -21,7 +22,7 @@ type FinancialMessageRequest struct {
 	LocalTransactionTime                   *field.String  `index:"12"`
 	LocalTransactionDate                   *field.String  `index:"13"`
 	CaptureDate                            *field.String  `index:"17"`
-	PointOfServiceConsitionCode            *field.String  `index:"25"`
+	PointOfServiceConditionCode            *field.String  `index:"25"`
 	AcquiringInstitutionIdentificationCode *field.String  `index:"32"`
 	RetrievalReferenceNumber               *field.String  `index:"37"`
 	CardAcceptorTerminalIdentification     *field.String  `index:"41"`
@@ -37,11 +38,99 @@ func (fmr *FinancialMessageRequest) PrettyPrint() string {
 	var builder strings.Builder
 	tw := tabwriter.NewWriter(&builder, 2, 2, 1, ' ', 0)
 
-	fmt.Fprintf(tw, "MTI\t%s\n", fmr.MTI.Value)
-	fmt.Fprintf(tw, "PrimaryAccountNumber\t%d\n", fmr.PrimaryAccountNumber.Value)
-	fmt.Fprintf(tw, "ProcessingCode\t%s\n", fmr.ProcessingCode.Value)
-	fmt.Fprintf(tw, "TransactionAmount\t%d\n", fmr.TransactionAmount.Value)
-	fmt.Fprintf(tw, "TransmissionDateTime\t%s\n", fmr.TransmissionDateTime.Value)
+	cases := []struct {
+		Item   field.Field
+		Format string
+	}{
+		{
+			Item:   fmr.MTI,
+			Format: "MTI\t%s",
+		},
+		{
+			Item:   fmr.PrimaryAccountNumber,
+			Format: "PrimaryAccountNumber\t%d",
+		},
+		{
+			Item:   fmr.ProcessingCode,
+			Format: "ProcessingCode\t%s",
+		},
+		{
+			Item:   fmr.TransactionAmount,
+			Format: "TransactionAmount\t%d",
+		},
+		{
+			Item:   fmr.TransmissionDateTime,
+			Format: "TransmissionDateTime\t%s",
+		},
+		{
+			Item:   fmr.STAN,
+			Format: "STAN\t%d",
+		},
+		{
+			Item:   fmr.LocalTransactionTime,
+			Format: "LocalTransactionTime\t%s",
+		},
+		{
+			Item:   fmr.LocalTransactionDate,
+			Format: "LocalTransactionDate\t%s",
+		},
+		{
+			Item:   fmr.CaptureDate,
+			Format: "CaptureDate\t%s",
+		},
+		{
+			Item:   fmr.PointOfServiceConditionCode,
+			Format: "PointOfServiceConditionCode\t%s",
+		},
+		{
+			Item:   fmr.AcquiringInstitutionIdentificationCode,
+			Format: "AcquiringInstitutionIdentificationCode\t%s",
+		},
+		{
+			Item:   fmr.RetrievalReferenceNumber,
+			Format: "RetrievalReferenceNumber\t%s",
+		},
+		{
+			Item:   fmr.CardAcceptorTerminalIdentification,
+			Format: "CardAcceptorTerminalIdentification\t%s",
+		},
+		{
+			Item:   fmr.CardAcceptorNameLocation,
+			Format: "CardAcceptorNameLocation\t%s",
+		},
+		{
+			Item:   fmr.AdditionalData,
+			Format: "AdditionalData\t%s",
+		},
+		{
+			Item:   fmr.LoyaltyData,
+			Format: "LoyaltyData\t%s",
+		},
+		{
+			Item:   fmr.POSAdditionalData,
+			Format: "POSAdditionalData\t%s",
+		},
+	}
+
+	for _, c := range cases {
+		if c.Item == nil || reflect.ValueOf(c.Item).IsNil() {
+			fmt.Fprintln(tw, c.Format, "Field not found")
+			continue
+		}
+
+		switch item := c.Item.(type) {
+		case *field.String:
+			fmt.Fprintf(tw, c.Format, item.Value)
+			fmt.Fprintln(tw)
+			continue
+		case *field.Numeric:
+			fmt.Fprintf(tw, c.Format, item.Value)
+			fmt.Fprintln(tw)
+			continue
+		default:
+			continue
+		}
+	}
 
 	tw.Flush()
 
