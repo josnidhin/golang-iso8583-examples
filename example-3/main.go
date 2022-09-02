c704b9dd16ae1ecf4c54486d4144d18f4d68226d
Jose Nidhin <jose.nidhin@dtone.com>
2022-09-02 17:11:01 +0800

    Add a a client and server to demo connection handler

--- example-3/main.go
+++ example-3/main.go
@@ -4,9 +4,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/hex"
 	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"sync"
@@ -17,22 +17,24 @@ import (
 	"github.com/moov-io/iso8583"
 )
 
-type MockReadWriteCloser struct {
-	bytes.Buffer
-}
+const hexStr = "009d49534f303234303030303535303230303532333838303030303841303830303031303831313030393934313830303030303030313030303030333133313032383432343838373539313032363431303331333033313330303030303034303139393131304d4f4e353047415a4f582020204e456469736f6e203132333520202020202020202020204d6f6e746572726579202020204e4c204d58343834"
 
-func (rwc *MockReadWriteCloser) Close() error {
-	return nil
-}
+var sampleRawInput []byte
 
-var rwc *MockReadWriteCloser
-var conn *Connection
+func init() {
+	var err error
+	fnName := "main.init"
+
+	sampleRawInput, err = hex.DecodeString(hexStr)
+	if err != nil {
+		logger.Panicf("%s: raw input creation failed - %v", fnName, err)
+	}
+}
 
 func main() {
-	var err error
-	fnName := "main"
 	wg := &sync.WaitGroup{}
-	shutdownNotifier := make(chan int)
+	address := ":8080"
+	shutdownNotifier := make(chan struct{})
 
 	wg.Add(1)
 	go func() {
@@ -40,25 +42,99 @@ func main() {
 		signalHandler(shutdownNotifier)
 	}()
 
-	rwc = &MockReadWriteCloser{}
-
-	inMsgHandler := func(msg *iso8583.Message) {
-		printISOMsg(msg)
-	}
-
-	conn, err = NewConnection(rwc, Spec1HeaderSize, Spec1, MsgLenReader, MsgLenWriter, inMsgHandler)
-	if err != nil {
-		logger.Fatalf("%s: error creating connection - %v", fnName, err)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		startServer(shutdownNotifier, wg, address)
+	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		mockWriter(shutdownNotifier)
+		startClient(shutdownNotifier, address)
 	}()
 
-	conn.Done()
 	wg.Wait()
+}
+
+func signalHandler(shutdownNotifier chan struct{}) {
+	fnName := "main.signalHandler"
+
+	defer func() {
+		logger.Printf("%s: graceful shutdown initialised", fnName)
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case <-shutdownNotifier:
+		return
+	case <-sigChan:
+		close(shutdownNotifier)
+	}
+}
+
+func startServer(shutdownNotifier <-chan struct{}, wg *sync.WaitGroup, address string) {
+	fnName := "main.startServer"
+	network := "tcp"
+
+	tcpAddr, err := net.ResolveTCPAddr(network, address)
+	if err != nil {
+		logger.Printf("%s: address resolve failed - %v", fnName, err)
+		return
+	}
+
+	tcpListener, err := net.ListenTCP(network, tcpAddr)
+	if err != nil {
+		logger.Printf("%s: bind listener failed - %v", fnName, err)
+		return
+	}
+
+	go func() {
+		<-shutdownNotifier
+		tcpListener.Close()
+	}()
+	logger.Printf("%s: server listening on address - %s", fnName, tcpAddr)
+
+	for {
+		conn, err := tcpListener.AcceptTCP()
+		if err != nil {
+			select {
+			case <-shutdownNotifier:
+				return
+			default:
+				logger.Printf("%s: accept connection failed - %v", fnName, err)
+				break
+			}
+		}
+
+		logger.Printf("%s: new connection", fnName)
+		conn.SetKeepAlive(true)
+		conn.SetKeepAlivePeriod(60 * time.Second)
+
+		connHandler, err := NewConnectionHandler(conn, Spec1HeaderSize, Spec1, MsgLenReader, MsgLenWriter, inMsgHandler)
+		if err != nil {
+			logger.Fatalf("%s: error creating connection handler - %v", fnName, err)
+		}
+
+		wg.Add(1)
+		go func(conn *net.TCPConn, connHandler *ConnectionHandler) {
+			defer wg.Done()
+			<-shutdownNotifier
+
+			err := connHandler.Close()
+			if err != nil {
+				logger.Printf("%s: error closing connection handler - %v", fnName, err)
+			}
+
+			connHandler.Done()
+		}(conn, connHandler)
+	}
+}
+
+func inMsgHandler(msg *iso8583.Message) {
+	printISOMsg(msg)
 }
 
 func printISOMsg(msg *iso8583.Message) {
@@ -84,44 +160,46 @@ func printISOMsg(msg *iso8583.Message) {
 	fmt.Println()
 }
 
-func signalHandler(shutdownNotifier chan int) {
-	fnName := "main.signalHandler"
+func startClient(shutdownNotifier <-chan struct{}, address string) {
+	var tcpConn *net.TCPConn
+	fnName := "main.startClient"
+	network := "tcp"
 
-	defer func() {
-		logger.Printf("%s: graceful shutdown initialised", fnName)
-	}()
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
-	select {
-	case <-shutdownNotifier:
-		return
-	case <-sigChan:
-		conn.Close()
-		close(shutdownNotifier)
-	}
-}
-
-func mockWriter(shutdownNotifier <-chan int) {
-	fnName := "main.mockWriter"
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	hexStr := "009d49534f303234303030303535303230303532333838303030303841303830303031303831313030393934313830303030303030313030303030333133313032383432343838373539313032363431303331333033313330303030303034303139393131304d4f4e353047415a4f582020204e456469736f6e203132333520202020202020202020204d6f6e746572726579202020204e4c204d58343834"
-	rawInput, err := hex.DecodeString(hexStr)
+	tcpAddr, err := net.ResolveTCPAddr(network, address)
 	if err != nil {
-		logger.Fatalf("%s: raw input creation error - %v", fnName, err)
+		logger.Printf("%s: address resolve failed - %v", fnName, err)
+		return
 	}
+
+	ticker := time.NewTicker(1 * time.Second)
+
+loop:
+	for {
+		select {
+		case <-shutdownNotifier:
+			break loop
+		case <-ticker.C:
+			tcpConn, err = net.DialTCP(network, nil, tcpAddr)
+			if err != nil {
+				logger.Printf("%s: dial failed - %v", fnName, err)
+				break
+			}
+
+			break loop
+		}
+	}
+
+	defer tcpConn.Close()
+	defer ticker.Stop()
 
 	for {
 		select {
 		case <-shutdownNotifier:
 			return
 		case <-ticker.C:
-			_, err = rwc.Write(rawInput)
+			_, err = tcpConn.Write(sampleRawInput)
 			if err != nil {
-				logger.Fatalf("%s: error while writing rawInput into rwc - %v", fnName, err)
+				logger.Printf("%s: error while writing to tcp connection - %v", fnName, err)
 			}
 		}
 	}
