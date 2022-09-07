8ca05a76c724c22633f1def7579456b59405128d
Jose Nidhin <jose.nidhin@dtone.com>
2022-09-07 14:56:20 +0800

    Refactor server code

--- example-3/main.go
+++ example-3/main.go
@@ -13,10 +13,7 @@ import (
 	"strings"
 	"sync"
 	"syscall"
-	"text/tabwriter"
 	"time"
-
-	"github.com/moov-io/iso8583"
 )
 
 const (
@@ -53,22 +50,36 @@ func main() {
 		signalHandler(shutdownNotifier)
 	}()
 
-	wg.Add(1)
+	var err error
+	var server *Server
+
+	go func() {
+		<-shutdownNotifier
+
+		if server != nil {
+			server.Shutdown()
+			wg.Done()
+		}
+	}()
+
 	switch mode {
 	case serverMode:
-		go func() {
-			defer wg.Done()
-			startServer(shutdownNotifier, wg, address)
-		}()
+		server, err = NewServer(address)
+		if err != nil {
+			logger.Fatalf("%v", err)
+		}
+
+		wg.Add(1)
+		server.Start()
 
 	case clientMode:
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			startClient(shutdownNotifier, address)
 		}()
 
 	default:
-		wg.Done()
 		fmt.Printf("Unkown mode - %s\n", mode)
 		os.Exit(1)
 	}
@@ -91,63 +102,6 @@ func signalHandler(shutdownNotifier chan struct{}) {
 		return
 	case <-sigChan:
 		close(shutdownNotifier)
-	}
-}
-
-func startServer(shutdownNotifier <-chan struct{}, wg *sync.WaitGroup, address string) {
-	fnName := "main.startServer"
-	network := "tcp"
-
-	tcpAddr, err := net.ResolveTCPAddr(network, address)
-	if err != nil {
-		logger.Printf("%s: address resolve failed - %v", fnName, err)
-		return
-	}
-
-	tcpListener, err := net.ListenTCP(network, tcpAddr)
-	if err != nil {
-		logger.Printf("%s: bind listener failed - %v", fnName, err)
-		return
-	}
-
-	go func() {
-		<-shutdownNotifier
-		tcpListener.Close()
-	}()
-	logger.Printf("%s: server listening on address - %s", fnName, tcpAddr)
-
-	for {
-		conn, err := tcpListener.AcceptTCP()
-		if err != nil {
-			select {
-			case <-shutdownNotifier:
-				return
-			default:
-				logger.Printf("%s: accept connection failed - %v", fnName, err)
-				break
-			}
-		}
-
-		logger.Printf("%s: new connection", fnName)
-
-		connHandler, err := NewConnectionHandler(conn, Spec1HeaderSize, Spec1, MsgLenReader, MsgLenWriter, inMsgHandler)
-		connHandler.Start()
-		if err != nil {
-			logger.Fatalf("%s: error creating connection handler - %v", fnName, err)
-		}
-
-		wg.Add(1)
-		go func(connHandler *ConnectionHandler) {
-			defer wg.Done()
-			<-shutdownNotifier
-
-			err := connHandler.Close()
-			if err != nil {
-				logger.Printf("%s: error closing connection handler - %v", fnName, err)
-			}
-
-			connHandler.Done()
-		}(connHandler)
 	}
 }
 
@@ -197,31 +151,4 @@ loop:
 			}
 		}
 	}
-}
-
-func inMsgHandler(msg *iso8583.Message) {
-	printISOMsg(msg)
-}
-
-func printISOMsg(msg *iso8583.Message) {
-	tw := tabwriter.NewWriter(os.Stdout, 2, 2, 1, ' ', 0)
-
-	for pos := 0; pos < 128; pos++ {
-		value, err := msg.GetString(pos)
-
-		if err != nil {
-			continue
-		}
-
-		if value == "" {
-			continue
-		}
-
-		field := msg.GetField(pos)
-
-		fmt.Fprintf(tw, "%3d\t%s\t%s\n", pos, field.Spec().Description, value)
-	}
-	tw.Flush()
-
-	fmt.Println()
 }
